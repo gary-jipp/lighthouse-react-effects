81c203fa1513949bed3d12365324230213e6ecd4
Gary J <github@accu.ca>
2021-10-27 22:07:22 -0700

    Update

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -2,18 +2,22 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 const Quote = function() {
-  const [data, setData] = useState();
+  const [data, setData] = useState({});
+  const [count, setCount] = useState(1);
 
   useEffect(() => {
     axios.get("https://api.kanye.rest")
       .then(res => {
-        console.log(res.data);
+        // console.log(res.data);
         setData(res.data);
       });
-  }, []);
+  }, [count]);
 
   return (
     <div>
+      <div>
+        <button onClick={() => setCount(!count)}>+</button>
+      </div>
       {data.quote}
     </div>
   );
