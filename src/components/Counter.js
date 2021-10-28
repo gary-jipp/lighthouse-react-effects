772d6664122310b92301840443b80287a3be549f
Gary J <github@accu.ca>
2021-10-27 21:21:31 -0700

    Update

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Counter = function() {
   const [count, setCount] = useState(0);
+
+
+  useEffect(() => {
+    document.title = ` Counter = ${count}`;
+  }
+  );
+
 
   return (
     <div>
       {count}
       <div>
-        <button onClick={() => setCount(count + 1)}>+</button> </div>
+        <button onClick={() => setCount(count + 1)}>+</button>
+      </div>
     </div>
   );
 };
