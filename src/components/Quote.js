3765d27907918f92ae283a9e5df3d4d0c015e97c
Gary J <github@accu.ca>
2021-10-27 22:01:33 -0700

    Update

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -2,13 +2,15 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 const Quote = function() {
+  const [data, setData] = useState();
 
   useEffect(() => {
     axios.get("https://api.kanye.rest")
       .then(res => {
         console.log(res.data);
+        setData(res.data);
       });
-  });
+  }, []);
 
   return (
     <div>
