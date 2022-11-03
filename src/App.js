36f577d64c262c3e3253ae3b805220c66afbadff
Gary J <github@accu.ca>
2022-11-03 09:46:26 -0700

    Update

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import {useState} from 'react';
 import 'App.css';
 
 export default function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <h2>React Effects</h2>
-      {count}
-      <button onClick={() => setCount(count + 1)}>+</button>
     </div>
   );
 }
